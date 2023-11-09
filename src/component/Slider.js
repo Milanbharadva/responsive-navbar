5f4208cf6e460c40136aa4505800a733b14573b1
Milanbharadva <96285463+Milanbharadva@users.noreply.github.com>
2023-11-09 11:27:31 +0530

    changed

--- src/component/Slider.js
+++ src/component/Slider.js
@@ -1,6 +1,7 @@
 const Slider = () => {
   function nav(e) {
     console.log(e.target);
+    // document.getElementById("navbar").classList.toggle("hidden");
     document.getElementById("navbar").classList.toggle("-translate-y-96");
   }
   return (
@@ -21,15 +22,15 @@ const Slider = () => {
           </li>
         </ul>
         <div
-          className="pt-5 pl-5 pb-5   bg-black space-y-1 block md:hidden "
+          className="pt-5 pl-5 pb-5 bg-black space-y-1 block md:hidden "
           onClick={nav}
         >
           <span className=" block mb-1 h-1.5 w-7 bg-white"></span>
           <span className="block h-1.5 mb-1 w-7 bg-white"></span>
           <span className="block h-1.5 w-7 bg-white"> </span>
         </div>
-        <div className="duration-1000  " id="navbar">
-          <ul className=" flex flex-col w-[100vw] bg-black pl-5 text-white space-y-4 pb-5 justify-around">
+        <div className="md:hidden duration-1000" id="navbar">
+          <ul className="flex flex-col w-[100vw] bg-black pl-5 text-white space-y-4 pb-5 justify-around">
             <li>
               <a href="">HOME</a>
             </li>
